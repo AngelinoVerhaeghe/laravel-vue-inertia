eafb76fc9e84750441e1eb63f863feb2c53f6fc2
Angelino Verhaeghe <verhaegheangelino@gmail.com>
2026-03-22 11:16:11 +0100

    first commit

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <section>
+        <div class="container mx-auto px-4 py-12">
+            <h1 class="text-4xl font-bold text-center">Welcome</h1>
+        </div>
+    </section>
+</template>
